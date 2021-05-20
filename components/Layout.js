2ae42231d06001706646d44017688f108d59b448
Kenny J <k-tech19@Kennys-Mac-mini.local>
2021-05-20 19:29:44 -0400

    inital set up

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+// import styles from '@/styles/Layout.module.css'
+
+
+
+export default function Layout() {
+    return (
+        <div>
+            <h1>Header</h1>
+
+        </div>
+    )
+}
---
+++ pages/projects.js
@@ -0,0 +1,26 @@
+import Link from 'next/Link'
+
+
+export default function ProjectPage() {
+    
+    
+
+
+    return (
+               
+
+
+        <div>
+            <Link href='/'> 
+                <a>Home</a>
+            </Link>
+
+            <h2> Projects </h2>
+            <h3>Black Star Anime</h3>
+            <p>This is a fullstack application that assists anime watchers keep track of their favorite anime show and movies.</p>
+            <button>Click Here </button>
+            <a href="https://black-star-anime.herokuapp.com/auth/login">Live Site </a>
+            <a href="">GitHub </a>
+        </div>
+    )
+}
