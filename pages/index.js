2d23b91cf98c0ac2720044b7b3174231f9fc7363
Kenny J <k-tech19@Kennys-Mac-mini.local>
2021-05-21 09:01:28 -0400

    inital project info added

--- pages/index.js
+++ pages/index.js
@@ -3,28 +3,48 @@ import Link from 'next/link'
 import Header from '@/components/Header'
 // import Footer from '@/components/Footer'
 import styles from '@/styles/Welcome.module.css'
+import { FaGithubSquare } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaTwitterSquare } from 'react-icons/fa'
 
 
 
 
 export default function Home() {
   return (
-      <div>
 
+    <div className={styles.main}>
        <Header />
         <h1 className={styles.welcome}>
           I'm Kenny Jean
         </h1>
 
+        <div className={styles.content}>
         <p>Welcome to my off portfolio website to showcase all work related to web development.</p>
-        <button>
+        <a className={styles.btn} href="#"> Download Resume </a>
+        </div>
 
-        </button>
+        <div className={styles.link_btn} >
+        <Link href="https://github.com/K-Tech19" target="_blank">
+               <a target="_blank"> <FaGithubSquare></FaGithubSquare> </a>
+           </Link>
+
+
+           <Link href="https://www.linkedin.com/in/kennyjean19/">
+               <a target="_blank"> <FaLinkedin></FaLinkedin> </a>
+           </Link>
+
+
+           <Link href="https://twitter.com/k_Tech19">
+               <a target="_blank"> <FaTwitterSquare></FaTwitterSquare> </a>
+           </Link>
         
 
+        </div>
+     </div>
 
 
-      {/* <Footer/> */}
-      </div>
+
+
   )
 }
--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,6 @@
 import Link from 'next/Link'
 import Header from '@/components/Header'
+import styles from '@/styles/Project.module.css'
 
 
 export default function ProjectPage() {
@@ -14,14 +15,38 @@ export default function ProjectPage() {
         <div>
 
             <Header/>   
-          
-
+          <div className={styles.title}>
             <h2> Projects </h2>
+
+          </div>
+          
+        <div className={styles.p1}>
             <h3>Black Star Anime</h3>
             <p>This is a fullstack application that assists anime watchers keep track of their favorite anime show and movies.</p>
-            <button>Click Here </button>
             <a href="https://black-star-anime.herokuapp.com/auth/login">Live Site </a>
             <a href="https://github.com/K-Tech19/black_star">GitHub </a>
+        </div>
+
+        <div className={styles.p2}>
+            <h3>Rapid Connect 4</h3>
+            <p>This is a vanilla Javascript app bulit with Javascript, HTML, and CSS </p>
+            <a href="https://k-tech19.github.io/P1-connect4/">Live Site </a>
+            <a href="https://github.com/K-Tech19/P1-connect4">GitHub </a>
+        </div>
+
+        <div className={styles.p3}>
+            <h3>GameOverFlow</h3>
+            <p>This is a group-based fullstack application that assists newcomers to a specific game ask questions to pros about the game.</p>
+            <a href="https://gameoverflow-client.herokuapp.com/">Live Site </a>
+            <a href="https://github.com/K-Tech19/gof-mern-client">GitHub </a>
+        </div>
+
+        <div className={styles.p4}>
+            <h3>Covid-tracker 2021</h3>
+            <p>This is a React single-paged application that displays up to date  Covid19 numbers of confirmed, recovered, and deaths for each country in the global.</p>
+            <a href="https://k-covid-19-tracker.netlify.app/">Live Site </a>
+            <a href="https://github.com/K-Tech19/covid_tracker2021">GitHub </a>
+            </div>
         </div>
     )
 }
